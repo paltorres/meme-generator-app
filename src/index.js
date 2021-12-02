53f2ed4a16fb0c54b07c44040d68c18434996c60
yailperalta-santex <yail.peralta@santexgroup.com>
2021-12-02 12:28:57 -0300

    Routes directory

--- src/index.js
+++ src/index.js
@@ -2,29 +2,13 @@ require('dotenv').config();
 
 const { App } = require('@slack/bolt');
 const { registerListeners } = require('./listeners');
+const { getRoutes } = require('./routes');
 
 // Initialize Bolt app, using the default HTTPReceiver
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  customRoutes: [
-    {
-      path: '/',
-      method: ['POST'],
-      handler: (req, res) => {
-        res.writeHead(200);
-        res.end('Health check information displayed here!');
-      },
-    },
-    {
-      path: '/memes',
-      method: ['GET'],
-      handler: (req, res) => {
-        res.writeHead(200);
-        res.end([]);
-      }
-    }
-  ],
+  customRoutes: getRoutes()
 });
 
 registerListeners(app);
